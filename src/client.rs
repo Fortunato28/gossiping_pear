03cb12fe529246e55ed47e5f6fb7c5d93745988a
Anton Sapunov <fort.sav.28@gmail.com>
2022-03-23 02:14:23 +0300

    So third peer recieves other peers now

--- src/client.rs
+++ src/client.rs
@@ -1,7 +1,8 @@
 use futures::{future, pin_mut, StreamExt, TryStreamExt};
 use futures_channel::mpsc::{unbounded, UnboundedSender};
+use itertools::Itertools;
 use tokio_tungstenite::connect_async;
-use tungstenite::Message;
+use tungstenite::{Error, Message};
 
 use crate::{gossiping::gossiping, nothing_to_do};
 
@@ -27,18 +28,19 @@ async fn client_behavior(period: u32, connection: String, port: u16) {
     //let peers_to_connect = incoming.next().await.unwrap().unwrap().to_string();
     //dbg!(&peers_to_connect);
 
-    // TODO probably extract shared incoming behavior with server part
-    let broadcast_incoming = incoming.try_for_each(|msg| {
-        println!(
-            "Received a message from {}: {}",
-            connection,
-            msg.to_text().unwrap()
-        );
+    //// TODO probably extract shared incoming behavior with server part
+    //let broadcast_incoming = incoming.try_for_each(move |msg| {
+    //    let connection = connection.clone();
+    //    println!(
+    //        "Received a message from {}: {}",
+    //        connection,
+    //        msg.to_text().unwrap()
+    //    );
 
-        future::ok(())
-    });
+    //    future::ok(())
+    //});
 
-    //let broadcast_incoming = receive_data(incoming, connection);
+    let broadcast_incoming = receive_data(incoming, connection, port);
 
     tokio::spawn(gossiping(tx.clone(), period));
     send_server_port(tx, port).await;
@@ -49,15 +51,43 @@ async fn client_behavior(period: u32, connection: String, port: u16) {
     future::select(broadcast_incoming, sending_process).await;
 }
 
-fn receive_data(
-    incoming: futures::stream::SplitStream<
+async fn receive_data(
+    mut incoming: futures::stream::SplitStream<
         tokio_tungstenite::WebSocketStream<
             tokio_tungstenite::MaybeTlsStream<tokio::net::TcpStream>,
         >,
     >,
     connection: String,
-) -> () {
-    todo!()
+    port: u16,
+) -> Result<(), Error> {
+    let peers_to_connect = incoming.next().await.unwrap().unwrap().to_string();
+    dbg!(&peers_to_connect);
+    let peers_to_connect = peers_to_connect
+        .split_whitespace()
+        .into_iter()
+        .map(|address| address.to_string())
+        .collect_vec();
+    //.for_each(|address| {
+    //    dbg!(address);
+    //    tokio::spawn(client_behavior(2, address.to_string(), port));
+    //});
+
+    //for address in peers_to_connect {
+    //    tokio::spawn(client_behavior(2, address.to_string(), port));
+    //}
+
+    // TODO probably extract shared incoming behavior with server part
+    let broadcast_incoming = incoming.try_for_each(move |msg| {
+        println!(
+            "Received a message from {}: {}",
+            connection,
+            msg.to_text().unwrap()
+        );
+
+        future::ok(())
+    });
+
+    broadcast_incoming.await
 }
 
 pub fn run_client(period: u32, connection: Vec<String>, port: u16) {
